2f2560f7749f277841421679cc19de30872d881d
auriel grant <aurgrant@gmail.com>
2023-04-14 14:38:46 -0500

    first commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,12 @@
+public class MyFirstClass {
+//shortcut for main is psvm + tab or main + tab
+
+   public static void main(String[] args){
+//       this will log stuff in your command line ln stands for new line
+//       you must jput a semi colon after every statement
+//       system.out.println is essentially how you console log in java
+//       short cut for this is sout + tab
+
+        System.out.println("Hello, welcome to Java !");
+   }
+}
